c3690cc75f2b3c7a307905437f53f4b0ee0194a5
SyntaxisTaxsyn <66139375+SyntaxisTaxsyn@users.noreply.github.com>
2024-12-26 16:36:02 +0000

    sw.js aktualisieren

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/github-page-pwa';
 var APP_PREFIX = 'SD Calculator_';
-var VERSION = 'version_008';
+var VERSION = 'version_009';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
