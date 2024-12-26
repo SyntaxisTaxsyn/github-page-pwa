45b119c2d29f9f75d376d4653cfacc308d9777da
SyntaxisTaxsyn <66139375+SyntaxisTaxsyn@users.noreply.github.com>
2024-12-26 15:38:14 +0000

    sw.js aktualisieren

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/github-page-pwa';
 var APP_PREFIX = 'gppwa_';
-var VERSION = 'version_004';
+var VERSION = 'version_005';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
